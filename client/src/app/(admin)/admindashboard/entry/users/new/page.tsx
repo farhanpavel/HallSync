73e9b8dc83bf78d429509353b0057f6699c73d44
farhanpavel <farhanpavel3@gmail.com>
2024-08-28 22:01:57 +0600

    new user function added

--- client/src/app/(admin)/admindashboard/entry/users/new/page.tsx
+++ client/src/app/(admin)/admindashboard/entry/users/new/page.tsx
@@ -1,6 +1,9 @@
-import * as React from "react";
-
+"use client";
+import React, { useState } from "react";
+import { url } from "@/components/Url/page";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -20,6 +23,43 @@ import {
 } from "@/components/ui/select";
 
 export default function Page() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "",
+  });
+  const [isLoading, setLoading] = useState(true);
+  const router = useRouter();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const handleRoleChange = (role: string) => {
+    setUser({ ...user, role });
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${url}/api/user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      if (!response.ok) {
+        alert("Server Error");
+        throw new Error("Failed to submit data");
+      } else {
+        setLoading(false);
+        setTimeout(() => {
+          router.back();
+        }, 3000);
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
   return (
     <Card>
       <CardHeader className="space-y-4">
@@ -30,44 +70,81 @@ export default function Page() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col w-1/4 space-y-1.5">
+            <div className="flex flex-col w-1/4 space-y-3">
               <Label htmlFor="framework">Select role</Label>
-              <Select>
+              <Select required onValueChange={handleRoleChange}>
                 <SelectTrigger id="framework">
                   <SelectValue placeholder="Unassigned" />
                 </SelectTrigger>
                 <SelectContent position="popper">
-                  <SelectItem value="astro">Hall Provost</SelectItem>
-                  <SelectItem value="nuxt">Student</SelectItem>
+                  <SelectItem value="provost">Hall Provost</SelectItem>
+                  <SelectItem value="student">Student</SelectItem>
                 </SelectContent>
               </Select>
             </div>
-            <div className="flex flex-col space-y-1.5">
+            <div className="flex flex-col space-y-6">
               <div>
                 <h1>Enter User Informations</h1>
               </div>
-              <div>
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" type="name" />
-              </div>
-              <div>
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" />
-              </div>
-              <div>
-                <Label htmlFor="password">Password</Label>
-                <Input type="password" id="password" />
+              <div className="space-y-2">
+                <div className="space-y-2">
+                  <Label htmlFor="name">Name</Label>
+                  <Input
+                    id="name"
+                    type="name"
+                    className="w-1/2"
+                    name="name"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    className="w-1/2"
+                    name="email"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    className="w-1/2"
+                    name="password"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
               </div>
             </div>
           </div>
+          <CardFooter className="flex justify-end mt-7">
+            {isLoading ? (
+              <Button
+                type="submit"
+                className=" bg-blue-500 hover:bg-blue-400 hover:transition-all hover:delay-100"
+              >
+                Submit
+              </Button>
+            ) : (
+              <Button
+                type="submit"
+                disabled
+                className=" bg-blue-500 hover:bg-blue-400 hover:transition-all hover:delay-100"
+              >
+                <Loader2 className="h-4 w-4 animate-spin" />
+              </Button>
+            )}
+          </CardFooter>
         </form>
       </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
     </Card>
   );
 }
